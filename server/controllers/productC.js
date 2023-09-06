6515e7513a7c35598c2e3524b0d54f2147dacc08
bilal <bilalrasheedw@gmail.com>
2023-09-06 12:02:35 +0530

    added search filter and pagination

--- server/controllers/productC.js
+++ server/controllers/productC.js
@@ -1,13 +1,20 @@
 import { asyncHandler, customError } from "../error/globalError.js";
 import Product from "../models/ProductM.js";
+import ApiFeatures from "../utils/apiFeatures.js";
 
 const getAllProducts = asyncHandler(async (req, res, next) => {
-  const product = await Product.find();
-  const numOfProduct = await Product.countDocuments();
-  res.status(200).json({ numOfProduct, product });
+  const totalProducts = await Product.countDocuments();
+  const resultsPerPage = 5
+  const apiFeatures = new ApiFeatures(Product.find(), req.query)
+  .search()
+  .filter()
+  .pagination(resultsPerPage);
+  const products = await apiFeatures.query;
+  const filtered = products.length
+  res.status(200).json({ totalProducts,filtered, products });
 });
 
-const createProduct = asyncHandler(async (req, res, next) => {
+const createProduct = asyncHandler(async (req, res, next) =>  {
   const product = await Product.create(req.body);
   res.status(200).json({ product });
 });
@@ -32,7 +39,10 @@ const deleteProduct = asyncHandler(async (req, res, next) => {
   const productExist = await Product.findById(req.params.id);
   if (!productExist) throw new customError("Product not Found", 404);
   const product = await Product.findByIdAndDelete(req.params.id);
-  console.log("🚀 ~ file: productC.js:33 ~ deleteProduct ~ req.params.id:", req.params.id)
+  console.log(
+    "🚀 ~ file: productC.js:33 ~ deleteProduct ~ req.params.id:",
+    req.params.id
+  );
   if (!product) throw new customError("Product Not found", 404);
   res.status(200).json({ msg: "deleted successfully" });
 });
